eaa9227ae69116895c8514203362354da41a81f9
calvin <butler2018@hotmail.com>
2018-01-30 11:43:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/a18013001/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.student.a18013001;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+
+public class MainActivity extends AppCompatActivity {
+    LocationManager lm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+    }
+
+    public void click1(View v) {
+
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+//            public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                                                      int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this,new String[]{
+                ACCESS_FINE_LOCATION,ACCESS_COARSE_LOCATION},321
+            );
+            return;
+        }else {
+            startLoc();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==321)
+        {
+            if(grantResults.length >0
+                    && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+
+                startLoc();
+            }else {
+
+            }
+        }
+    }
+
+    private void startLoc() {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new MyListener());
+    }
+}
+
+//監聽
+class MyListener implements LocationListener
+{
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+     Log.d("LOC","Change!!" + location.getLatitude()+","+ location.getLongitude());
+
+     //計算到101的距離
+     Location loc101 = new Location("LOC");
+     loc101.setLatitude(25.0336);  //緯度
+     loc101.setLongitude(121.5646); //經度
+
+     float dist = location.distanceTo(loc101);
+     Log.d("LOC","dist:" +dist);
+
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
